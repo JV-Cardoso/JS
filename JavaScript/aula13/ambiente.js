9d5410dae6bd744a8549d4b8d56ca63baacaa67e
João Cardoso <joaovitorcardoso_1@outlook.com>
2023-06-28 11:03:09 -0300

    .

---
+++ JavaScript/aula13/ambiente.js
@@ -0,0 +1,11 @@
+/*var contador = 1
+while(contador <= 10){
+    console.log(`Hello World número: ${contador}`)
+    contador += 1
+}*/
+
+
+var contador = 1
+do {
+    console.log(`Hello World número: ${contador}`)
+    contador += 1} while (contador <= 10 )
